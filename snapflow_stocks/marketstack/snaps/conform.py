8e1ae837071f70f98f82892a9aa71a9302642a67
Ken Van Haren <kenvanharen@gmail.com>
2021-03-04 21:04:19 -0800

    Don't iterate for now

--- snapflow_stocks/marketstack/snaps/conform.py
+++ snapflow_stocks/marketstack/snaps/conform.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from pandas.core.frame import DataFrame
+
 from snapflow import DataBlock, Snap
 from snapflow.storage.data_formats.data_frame import DataFrameIterator
 
@@ -9,13 +11,24 @@ if TYPE_CHECKING:
     from snapflow_stocks import Ticker, MarketstackTicker
 
 
+# @Snap(
+#     "marketstack_conform_tickers",
+#     module="stocks",
+# )
+# def marketstack_conform_tickers(
+#     tickers: DataBlock[MarketstackTicker],
+# ) -> DataFrameIterator[Ticker]:
+#     for df in tickers.as_dataframe_iterator():
+#         df["exchange"] = df["exchange_acronym"]
+#         yield df[["symbol", "name", "exchange"]]
+
+
 @Snap(
-    "marketstack_conform_tickers",
-    module="stocks",
+    "marketstack_conform_tickers", module="stocks",
 )
 def marketstack_conform_tickers(
     tickers: DataBlock[MarketstackTicker],
-) -> DataFrameIterator[Ticker]:
-    for df in tickers.as_dataframe_iterator():
-        df["exchange"] = df["exchange_acronym"]
-        yield df[["symbol", "name", "exchange"]]
+) -> DataFrame[Ticker]:
+    df = tickers.as_dataframe()
+    df["exchange"] = df["exchange_acronym"]
+    return df[["symbol", "name", "exchange"]]
